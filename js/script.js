5cc26415a933e30187afbc2c8cbaabb8c6e2a0bc
gabezy <gabrielmoreira2705@gmail.com>
2022-12-07 21:23:20 -0300

    slide configs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 import Slide from "./slides.js";
 
 const slide = new Slide(".wrapper", ".slide").init();
+slide.changeSlide(4);
--- js/slides.js
+++ js/slides.js
@@ -15,6 +15,37 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return slide.offsetLeft - margin;
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return {
+        element,
+        position,
+      };
+    });
+  }
+
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(this.slideArray[index].position);
+    this.slideIndexNav(index);
+    this.dist.finalPosition = activeSlide.position;
+  }
+
   onStart(e) {
     let moveType;
     if (e.type === "mousedown") {
@@ -59,6 +90,7 @@ export default class Slide {
   init() {
     this.bindEvents();
     this.addSlideEvent();
+    this.slidesConfig();
     return this;
   }
 }
